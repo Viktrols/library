3e8b6d739f6d40d32c6e88aa1eb7634c763d08e6
Viktoria Lupanova <viktoriia.lupanova@gmail.com>
2023-05-26 16:54:13 +0200

    Len' pridumyvat'

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import './lib/lib';
 
-$('div').click(function() {
-    console.log($(this).index());
-});
+// $('div').click(function() {
+//     console.log($(this).index());
+// });
+
+// console.log($('div').find('.x'));
+
+$('.x').fadeIn(2000);
